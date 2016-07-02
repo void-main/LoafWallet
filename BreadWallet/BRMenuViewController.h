d65f74085de866192f6e014e449ecc8c34d855f6
Yifei Zhou <austinchou0126@gmail.com>
2016-07-02 19:34:18 +0800

    Added ECSlidingViewController and menu view controller

---
+++ BreadWallet/BRMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  BRMenuViewController.h
+//  LoafWallet
+//
+//  Created by Yifei Zhou on 7/2/16.
+//  Copyright © 2016 Aaron Voisine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BRMenuViewController : UIViewController
+
+@end
---
+++ BreadWallet/BRMenuViewController.m
@@ -0,0 +1,37 @@
+//
+//  BRMenuViewController.m
+//  LoafWallet
+//
+//  Created by Yifei Zhou on 7/2/16.
+//  Copyright © 2016 Aaron Voisine. All rights reserved.
+//
+
+#import "BRMenuViewController.h"
+
+@interface BRMenuViewController ()
+
+@end
+
+@implementation BRMenuViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
